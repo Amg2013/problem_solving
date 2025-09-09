d90f5d2b569437958d0b8641142cd991b3621eb7
AMGAD NADEY <81270359+Amg2013@users.noreply.github.com>
2025-09-09 19:46:12 +0300

    Time: 43 ms (28.12%), Space: 14.1 MB (63.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++) {
-
-            for(int j = i+1; j < nums.size(); j++) { 
-
-                 // j = i + 1 to avoid duplicate & self-pairing
-
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};  // return a vector with the indices
-                }
+        
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){  // Fixed condition here
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                } 
             }
         }
-        return {}; // return empty vector if no pair found
+        return {};
     }
 };
